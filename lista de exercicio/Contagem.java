9f8bfe96f7991f0aeeb0a3b44b827c59c1ad5dfd
heitorcn2302 <heitorcaetano886@gmail.com>
2025-11-24 15:59:46 -0300

    Add files via upload

---
+++ lista de exercicio/Contagem.java
@@ -0,0 +1,12 @@
+
+public class Contagem {
+   public static void main(String[] args) {
+	   
+	   int i = 0;
+	   
+	   while(i<=500) {
+		   System.out.print("  " +i);   
+		  i = i+2;
+	   }
+   }
+}
